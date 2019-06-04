ed3270082d17d4b15627f1d4ce392800a9dee4fc
Joseph Tubridy and Lisa Jensen <joseph@>
2019-06-04 14:50:51 -0700

    add all vocab and definition with toggle functions

---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+$(function() {
+  $("#java").click(function() {
+  $(".word").fadeToggle();
+  $(".definition").fadeToggle();
+  });
+
+  $("#op").click(function() {
+  $(".word1").fadeToggle();
+  $(".definition1").fadeToggle();
+  });
+
+  $("#varry").click(function() {
+  $(".word2").fadeToggle();
+  $(".definition2").fadeToggle();
+  });
+
+  $("#naming").click(function() {
+  $(".word3").fadeToggle();
+  $(".definition3").fadeToggle();
+  });
+
+  $("#functions").click(function() {
+  $(".word4").fadeToggle();
+  $(".definition4").fadeToggle();
+  });
+
+  $("#methods").click(function() {
+  $(".word5").fadeToggle();
+  $(".definition5").fadeToggle();
+  });
+  $("#arguments").click(function() {
+  $(".word6").fadeToggle();
+  $(".definition6").fadeToggle();
+  });
+  $("#parameters").click(function() {
+  $(".word7").fadeToggle();
+  $(".definition7").fadeToggle();
+  });
+  $("#return").click(function() {
+  $(".word8").fadeToggle();
+  $(".definition8").fadeToggle();
+  });
+  $("#chainingMethods").click(function() {
+  $(".word9").fadeToggle();
+  $(".definition9").fadeToggle();
+});
+  $("#strings").click(function() {
+  $(".word10").fadeToggle();
+  $(".definition10").fadeToggle();
+});
+  $("#booleans").click(function() {
+  $(".word11").fadeToggle();
+  $(".definition11").fadeToggle();
+  });
+  $("#undefined").click(function() {
+  $(".word12").fadeToggle();
+  $(".definition12").fadeToggle();
+  });
+  $("#nan").click(function() {
+  $(".word13").fadeToggle();
+  $(".definition13").fadeToggle();
+  });
+  $("#escape").click(function() {
+  $(".word14").fadeToggle();
+  $(".definition14").fadeToggle();
+  });
+  $("#alert").click(function() {
+  $(".word15").fadeToggle();
+  $(".definition15").fadeToggle();
+  });
+  $("#comments").click(function() {
+  $(".word16").fadeToggle();
+  $(".definition16").fadeToggle();
+  });
+  $("#jquery").click(function() {
+  $(".word17").fadeToggle();
+  $(".definition17").fadeToggle();
+  });
+  $("#attributes").click(function() {
+  $(".word18").fadeToggle();
+  $(".definition18").fadeToggle();
+  });
+});
